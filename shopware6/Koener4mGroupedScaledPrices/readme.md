44280a0c2f1cdaf859d1a928cf955857b9a55719
panravildev <69759709+panravildev@users.noreply.github.com>
2021-09-20 03:50:52 +0200

    create empty shopware 6 plugin - Koener4mGroupedScaledPrices

---
+++ shopware6/Koener4mGroupedScaledPrices/readme.md
@@ -0,0 +1,5 @@
+Change History:
+
+1.2.0
+- added configuration to the plugin
+- ignore change order status for the payment methods set on the configuration
---
+++ shopware6/Koener4mGroupedScaledPrices/src/Koener4mGroupedScaledPrices.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Koener4m\GroupedScaledPrices;
+
+use Shopware\Core\Framework\Plugin;
+
+class Koener4mGroupedScaledPrices extends Plugin
+{
+}
